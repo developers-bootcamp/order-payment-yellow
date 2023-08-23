f5c8a3dd54cba2a6a694a1916dca7d3060169339
MaayanShore <maayanshore@gmail.com>
2023-08-23 21:01:28 +0300

    model_basic

---
+++ src/main/java/com/sap/orderpaymentyellow/model/OrderPayment.java
@@ -0,0 +1,31 @@
+package com.sap.orderpaymentyellow.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@SuperBuilder(toBuilder = true)
+public class OrderPayment {
+
+    private String orderId;
+    private String userId;
+    private double amount;
+    private int invoiceNumber;
+    private PaymentType paymentType;
+    private AuditData auditData;
+
+    public enum PaymentType {
+        CREDIT,
+        DEBIT
+    }
+}
